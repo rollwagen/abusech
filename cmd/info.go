48985c24daf533f12dbed49406c923aac15379f6
rollwagen <rollwagen@gmail.com>
2023-03-23 17:58:17 +0100

    Use new improved logo

--- cmd/info.go
+++ cmd/info.go
@@ -6,26 +6,30 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// infoCmd description of the company.
+// infoCmd information about the CLI and abuse.ch
 var infoCmd = &cobra.Command{
 	Use:   "info",
-	Short: "Print information about the CLI and abuse.ch",
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("                                                                  ╞▌                 ")
-		fmt.Println("                                                                  ╞▌         █       ")
-		fmt.Println("         ██       █████▄▄▄    ██      ██    ▄▄████▄    ████████   ╞▌   ▄▄▀▀  █▄▄▄▄   ")
-		fmt.Println("        ████      ██    ██▌   ██      ██   ▐█▌         ██         ╞▌   █     █   █   ")
-		fmt.Println("       ██  █▌     ██▄▄▄▄██    ██      ██    ██▄▄▄      ██▄▄▄▄▄    ╞▌   ▀▀▄▄  █   █   ")
-		fmt.Println("      ██    █▌    ██▀▀▀▀▀█▌   ██      ██     ▀▀▀▀██▄   ██▀▀▀▀▀    ╞▌                 ")
-		fmt.Println("     ██▌     █▄   ██     ██    █      █           █▌   ██         ╞▌                 ")
-		fmt.Println("    ██▀      ▀█   █████▀▀▀     ▀▀████▀▀     ▀████▀▀    ████████   ╞▌                 ")
-		fmt.Println("                                                                  ╞▌                 ")
+	Short: "Output information about the CLI and abuse.ch",
+	Run: func(_ *cobra.Command, _ []string) {
+		fmt.Println("                                                       -              ")
+		fmt.Println("                                                       -          #   ")
+		fmt.Println("                                                       -          #   ")
+		fmt.Println("    ##      ######     #*     #=    #####   -######.   -     ###  ###+")
+		fmt.Println("   ####     ##   .##   #*     #=  *#.       -#-        -    *     #  #")
+		fmt.Println("   ##*#     ##    ##   #*     #=  ##.       -#-        -     ###  #  #")
+		fmt.Println("  ##  ##    #######    #*     #=   -#####   -######    -              ")
+		fmt.Println(" .##  +#:   ##   .##   #*     #=      :##*  -#-        -              ")
+		fmt.Println(" ##    ##   ##    ##   ##    +#=        ##  -#-        -              ")
+		fmt.Println("-#=    -#+  #######    .#######   #######   -#######   -              ")
+		fmt.Println("                                                       -              ")
 		fmt.Println(" ")
 		fmt.Println(" ")
-		fmt.Println("Abuse.ch is a community-driven threat intelligence provider that helps internet service providers and network")
-		fmt.Println("operators protect their infrastructure from cyber threats, with a focus on malware and botnets.")
+		fmt.Println("Abuse.ch is a community-driven threat intelligence provider that helps")
+		fmt.Println("internet service providers and network operators protect their infrastructure")
+		fmt.Println("from cyber threats, with a focus on malware and botnets.")
 		fmt.Println(" ")
-		fmt.Println("https://github.com/rollwagen/abusech")
+		fmt.Println("For further information see https://abuse.ch and https://threatfox.abuse.ch/api/")
+		fmt.Println(" ")
 	},
 }
 
